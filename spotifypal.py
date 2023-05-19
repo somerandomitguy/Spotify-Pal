3a74fbb6339b1cbe017effe742e2f3540984f514
Liam Palmgren <ashla@liams-mbp.rad.liberty.edu>
2023-05-18 22:12:14 -0400

    Finally some real code, removed 'hello world' aspect

--- spotifypal.py
+++ spotifypal.py
@@ -1,31 +1,54 @@
-# This example requires the 'message_content' intent.
-
-import discord 
+import discord
 import os
+import spotipy.oauth2 as oauth2
 from dotenv import load_dotenv
-from discord.ext import commands
+from spotipy.oauth2 import SpotifyOAuth
 
+# Load the environment variables
 load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+SPOTIFY_CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
+SPOTIFY_CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
+SPOTIFY_REDIRECT_URI = os.getenv('SPOTIFY_REDIRECT_URI')
+SPOTIFY_USERNAME = os.getenv('SPOTIFY_USERNAME')
+SPOTIFY_PLAYLIST_ID = os.getenv('SPOTIFY_PLAYLIST_ID')
 
-intents = discord.Intents.default()
-intents.message_content = True
+# Initialize the Discord bot and Spotify client
+bot = discord.Client()
+spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(
+    client_id=SPOTIFY_CLIENT_ID,
+    client_secret=SPOTIFY_CLIENT_SECRET,
+    redirect_uri=SPOTIFY_REDIRECT_URI,
+    scope='playlist-modify-private',
+    username=SPOTIFY_USERNAME
+))
 
-#client = discord.Client(intents=intents)
-bot = commands.Bot(command_prefix='!', intents=intents)
-
-#@client.event
 @bot.event
 async def on_ready():
-    print(f'We have logged in as {bot.user}')
+    print(f'Logged in as {bot.user.name} ({bot.user.id})')
 
-#@client.event
-@bot.command(name="start", help="Starts a pomodoro timer")
-async def start_timer(ctx):
-    await ctx.send("Time to work!")
-#    if message.author == client.user:
-#        return
-#    if message.content.startswith('$hello'):
-#        await message.channel.send('Hello!')
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
 
-#client.run(os.environ['BOT_TOKEN'])
-bot.run(os.environ['BOT_TOKEN'])
+    if message.content.startswith('!addsong'):
+        # Get the song name and artist from the message content
+        song_data = message.content.split(' ', 1)[1]
+        song_name, song_artist = song_data.split(' by ', 1)
+
+        # Search for the song on Spotify
+        results = spotify.search(q=f'track:{song_name} artist:{song_artist}', type='track')
+
+        if results['tracks']['items']:
+            track_uri = results['tracks']['items'][0]['uri']
+
+            # Add the song to the Spotify playlist
+            spotify.user_playlist_add_tracks(SPOTIFY_USERNAME, SPOTIFY_PLAYLIST_ID, [track_uri])
+
+            await message.channel.send('Song added to the Spotify playlist!')
+        else:
+            await message.channel.send('Song not found on Spotify.')
+
+# Start the Discord bot
+bot.run(TOKEN)
